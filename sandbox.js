404fc2dea1a596b8343d7129f4b7580f7ac10557
Aqib Rehman <aqibr777@gmail.com>
2022-05-29 16:53:43 +0500

    Compiling Typescript

--- sandbox.js
+++ sandbox.js
@@ -4,4 +4,5 @@ var inputs = document.querySelectorAll('input');
 console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
+    ;
 });
--- sandbox.ts
+++ sandbox.ts
@@ -7,5 +7,5 @@ const inputs = document.querySelectorAll('input');
 console.log(inputs);
 
 inputs.forEach(input => {
-    console.log(input);
+    console.log(input);;
 });
