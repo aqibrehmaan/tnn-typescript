5a47b515be5d5c7def47561f9ea6db334e1e45e7
Aqib Rehman <aqibr777@gmail.com>
2022-06-19 16:35:38 +0500

    Funtion Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,15 +1,30 @@
+// let greet: Function;
 
-type StringOrNum = string | number;
-type objWithName = { name: string, uid: StringOrNum };
+// example 1
 
-const logDetails = (uid: StringOrNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+
+    console.log(`${name} says ${greeting}`);
+
+}
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if(action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
 }
 
-const greet = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
-}
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
 
-const greetAgain = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
-}
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
