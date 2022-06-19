8340fd00a227615cb9350490db50e40939c09107
Aqib Rehman <aqibr777@gmail.com>
2022-06-19 16:17:55 +0500

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('test');
+// let greet = () => {
+//     console.log('hello, world');
+// }
+//greet = 'hello';
+let greet;
+greet = () => {
+    console.log('hello, again');
+};
+// c is optional parameter
+// const add = (a: number, b: number, c?: number | string) => {
+//     console.log(a + b);
+//     console.log(c); // undefined
+// }
+// Default 
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,32 @@
-console.log('test');
+// let greet = () => {
+//     console.log('hello, world');
+// }
+
+//greet = 'hello';
+
+let greet: Function;
+
+greet = () => {
+    console.log('hello, again');
+}
+
+// c is optional parameter
+// const add = (a: number, b: number, c?: number | string) => {
+//     console.log(a + b);
+//     console.log(c); // undefined
+// }
+
+// Default 
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b);
+    console.log(c);
+}
+
+
+add(5, 10);
+
+const minus = (a: number, b: number): number => {
+    return a + b;
+}
+ 
+let result = minus(10, 7);
